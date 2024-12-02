14e162c79ac827507bd3b37997989196c8a46a74
JHolland <20102340@mail.wit.ie>
2024-12-02 19:54:45 +0000

    Mongo lab Integrated Tasky React App

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -5,7 +5,7 @@ import { tasksData } from './tasksData';
 const router = express.Router();
 
 // Get all tasks
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
     res.json(tasksData);
 });
 
@@ -23,29 +23,23 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
 
-    // Create the new task with additional properties
     const newTask = {
-        id: uuidv4(), // Generate a unique ID
+        id: uuidv4(),
         title,
         description,
         deadline,
         priority,
         done,
-        created_at: new Date().toISOString(), // Timestamp when the task is created
-        updated_at: new Date().toISOString()  // Timestamp when the task is created
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString()
     };
 
-    // Add the new task to the data
     tasksData.tasks.push(newTask);
-
-    // Increment the total_results counter
     tasksData.total_results++;
-
-    // Respond with the newly created task
     res.status(201).json(newTask);
 });
 
-// Update an existing task
+// Update a task
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
@@ -54,18 +48,14 @@ router.put('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
 
-    // Merge existing task data with updated data from request body
     const updatedTask = {
-        ...tasksData.tasks[taskIndex],  // Keep the existing task data
-        ...req.body,                    // Update with new data from the request body
-        updated_at: new Date().toISOString(),  // Update the timestamp
-        id                             // Keep the original ID (unchanged)
+        ...tasksData.tasks[taskIndex],
+        ...req.body,
+        updated_at: new Date().toISOString(),
+        id
     };
 
-    // Update the task in tasksData
     tasksData.tasks[taskIndex] = updatedTask;
-
-    // Respond with the updated task
     res.json(updatedTask);
 });
 
@@ -78,13 +68,8 @@ router.delete('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
 
-    // Remove the task from tasksData
     tasksData.tasks.splice(taskIndex, 1);
-
-    // Decrease the total_results counter
     tasksData.total_results--;
-
-    // Respond with a no-content status
     res.status(204).send();
 });
 
